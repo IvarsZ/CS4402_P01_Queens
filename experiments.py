8b8423f2f555118675b6c8c6d565a37bd5a352a0
iz2 <devnull@localhost>
2014-03-11 14:59:20 +0000

    Different p1 impl

--- experiments.py
+++ experiments.py
@@ -1,62 +1,47 @@
 import subprocess
 import glob
 
-min = 2
-max = 6
-few_times = 1
-many_times = 1
-parts = ['p1', 'p2', 'p4']
-
-for m in range(min, max + 1):
-
-  times = 0
-  if m <= 7:
-    times = many_times
-  else:
-    times = few_times
+def run_experiment(part, m, times): 
 
   results_file = open("results.txt", "a")
-
-  for part in parts:
-    
-    input_file = 'src/m_queens_{0}.eprime'.format(part)
-    
-    number_of_queens = -1
-
-    for i in range(times):
-
-      # Run savilerow
-      output_file = 'solutions/{0}_queens_{1}_{2}.solution'.format(m, part, i)
-      args = './savilerow -in-eprime {0} -run-minion minion -out-solution {1}'.format(input_file, output_file).split() + ['-params', 'letting n be {0}'.format(m)]
-      subprocess.call(args)
-
-      # Read/write output
-      nodes = -1
-      time = -1
-      with open(output_file) as fp:
-        for line in fp:
-          words = line.split()
-          if len(words) >= 4:
-            if words[1] == 'numberOfQueens':
-              number_of_queens = words[3]
-            elif words[2] == 'TotalTime:':
-              time = words[3]
-            elif words[2] == 'Nodes:':
-              nodes = words[3]
-      #results_file.write(part + ' ' + m + ' ' + i + ' ' + time + ' ' + nodes + ' ' + number_of_queens)
-      results_file.write(' '.join(str(i) for i in [part, m, i, time, nodes, number_of_queens]) + '\n')
+  input_file = 'src/m_queens_{0}.eprime'.format(part)
   
-    
-    # Find the number of solutions
-    input_file = 'src/m_queens_{0}_all.eprime'.format(part)
-    output_file = 'solutions/{0}_queens_{1}_all.solution'.format(m, part)
+  number_of_queens = -1
+  
+  for i in range(times):
+  
+    # Run savilerow
+    output_file = 'solutions/{0}_queens_{1}_{2}.solution'.format(m, part, i)
+    args = './savilerow -in-eprime {0} -run-minion minion -out-solution {1}'.format(input_file, output_file).split() + ['-params', 'letting n be {0}'.format(m)]
+    subprocess.call(args)
+  
+    # Read/write output
+    nodes = -1
+    time = -1
+    with open(output_file) as fp:
+      for line in fp:
+        words = line.split()
+        if len(words) >= 4:
+          if words[1] == 'numberOfQueens':
+            number_of_queens = words[3]
+          elif words[2] == 'TotalTime:':
+            time = words[3]
+          elif words[2] == 'Nodes:':
+            nodes = words[3]
+    results_file.write(' '.join(str(i) for i in [part, m, i, time, nodes, number_of_queens]) + '\n')
+  
+  results_file.close()
+  
+  
+# Find the number of solutions for given m and number of queens.
+def findNumberOfSolutions(part, m, numberOfQueens):
+  
+    input_file = 'src/m_queens_{0}.eprime'.format(part)
+    output_file = 'solutions/{0}_queens_{1}.solution'.format(m, part)
     args = './savilerow -in-eprime {0} -run-minion minion -out-solution {1} -all-solutions'.format(input_file, output_file).split() + ['-params', 'letting n be {0}'.format(m)]
     subprocess.call(args)
-
-    # by counting files with minimal number of queens used
-    print output_file + "*"
-    print number_of_queens
-
+  
+    # by counting files with number of queens used
     number_of_solutions = 0
     solutions = glob.glob(output_file + "*")
     for solution in solutions: 
@@ -65,9 +50,20 @@ for m in range(min, max + 1):
           words = line.split()
           if words[1] == 'numberOfQueens' and words[3] == number_of_queens:
             number_of_solutions += 1
-
+  
     queens_file = open("number_of_solutions.txt", "a")
     queens_file.write(' '.join(str(i) for i in [part, m, number_of_solutions]) + '\n')
     queens_file.close()
- 
-  results_file.close()
+
+# DEFINE what experiments to run.
+def compare_sum_atleast_exists_for_p1():
+  for m in range(5, 13):
+    for part in ['p1_sum', 'p1_atleast', 'p1_exists']:
+      run_experiment(part, m, 1)
+#compare_sum_atleast_exists_for_p1()
+
+def compare_sum_atleast_exists_explicit_for_p2():
+  for m in range(5, 13):
+    for part in ['p2_explicit', 'p2_sum', 'p2_atleast', 'p2_exists']:
+      run_experiment(part, m, 1)
+compare_sum_atleast_exists_explicit_for_p2()
--- graphs.r
+++ graphs.r
@@ -1,10 +1,6 @@
 queries <- read.table("results.txt", head = TRUE, sep=" ", fill=TRUE)
 
-p <- 0
-zero <-c(0)
-p_range <-c('p1', 'p2', 'p4')
-c_range <-c(1, 2, 3)
-
+# Collect results, calcute mean value for times and errors.
 q <- aggregate(queries[c("i", "time", "nodes", "queens")], by=queries[c("part", "m")], FUN=mean)
 q$time <- round(q$time, 3)
 
@@ -23,6 +19,28 @@ q$absError <- q$time - q$cilLow
 q$relError <- round(q$absError / q$time, 1) * 100
 
 q$cilUp <- q$time + q$absError
+
+plot_parts_time <- function(p_range, name, x_max, y_max) {
+  
+  p <- 0
+  zero <-c(0)
+  c_range <-c(seq(1, length(p_range)))
+  plot(zero, zero, xlab="m", ylab="Time, s", log="y", main=name, pch=p, xlim=c(0, x_max), ylim=c(0.001, y_max), yaxt="n")
+  for (c in c_range) {
+    p_set <- subset(q, q$part == p_range[c])
+    lines(p_set$m, p_set$time, col=c)
+    points(p_set$m, p_set$time, col=c)
+  }
+  legend("topleft", title="parts", legend=p_range, col=c_range, cex=0.5, pch=1)
+  aty <- axTicks(2)
+  labels <- sapply(aty,function(i)
+    as.expression(bquote(.(i)))
+  )
+  axis(2,at=aty,labels=labels)
+}
+
+# Plot different p1 implementations.
+plot_parts_time(c('p1_atleast', 'p1_sum', 'p1_exists'), "Different p1 implementations", 13, 1000)
 
 plot(zero, zero, xlab="m", ylab="Time, s", log="y", main="Solving time for different parts", pch=p, xlim=c(0, 13), ylim=c(0.001, 200), yaxt="n")
 for (c in c_range) {
