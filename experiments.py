82702420a8d345e0c4bf7f57cc00714cb2bc77a5
iz2 <devnull@localhost>
2014-03-12 02:06:30 +0000

    Report

--- experiments.py
+++ experiments.py
@@ -12,7 +12,7 @@ def run_experiment(part, m, times):
   
     # Run savilerow
     output_file = 'solutions/{0}_queens_{1}_{2}.solution'.format(m, part, i)
-    args = './savilerow -in-eprime {0} -run-minion minion -out-solution {1}'.format(input_file, output_file).split() + ['-mberOfQueensparams', 'letting m be {0}'.format(m)]
+    args = './savilerow -in-eprime {0} -run-minion minion -out-solution {1}'.format(input_file, output_file).split() + ['-params', 'letting m be {0}'.format(m)]
     subprocess.call(args)
   
     # read/write output
@@ -22,16 +22,15 @@ def run_experiment(part, m, times):
       for line in fp:
         words = line.split()
         if len(words) >= 4:
-          if words[1] == 'numberofqueens':
+          if words[1] == 'numberOfQueens':
             number_of_queens = words[3]
-          elif words[2] == 'totaltime:':
+          elif words[2] == 'TotalTime:':
             time = words[3]
-          elif words[2] == 'nodes:':
+          elif words[2] == 'Nodes:':
             nodes = words[3]
     results_file.write(' '.join(str(i) for i in [part, m, i, time, nodes, number_of_queens]) + '\n')
   
   results_file.close()
-  
   
 # find the number of solutions for given m and number of queens.
 def find_number_of_solutions(part, m, number_of_queens):
@@ -63,48 +62,47 @@ def compare_sum_atleast_exists_for_p1():
   for m in range(5, 13):
     for part in ['p1_sum', 'p1_atleast', 'p1_exists']:
       run_experiment(part, m, 1)
-#compare_sum_atleast_exists_for_p1()
+compare_sum_atleast_exists_for_p1()
 
 def compare_sum_atleast_exists_explicit_for_p2():
   for m in range(5, 12):
     for part in ['p2_explicit', 'p2_sum', 'p2_atleast', 'p2_exists']:
       run_experiment(part, m, 1)
-#compare_sum_atleast_exists_explicit_for_p2()
-
+compare_sum_atleast_exists_explicit_for_p2()
 
 def compare_heuristics_for_p1():
   for m in range(5, 13):
     for part in ['p1_atleast','p1_static', 'p1_sdf', 'p1_conflict', 'p1_srf']:
       run_experiment(part, m, 1)
-#compare_heuristics_for_p1()
+compare_heuristics_for_p1()
 
 def compare_heuristics_for_p2():
   for m in range(5, 13):
     for part in ['p2_sum', 'p2_static', 'p2_sdf', 'p2_conflict', 'p2_srf']:
       run_experiment(part, m, 1)
-#compare_heuristics_for_p2()
+compare_heuristics_for_p2()
 
 def compare_heuristics_for_p4():
   for m in range(5, 13):
     for part in ['p4_atleast', 'p4_static', 'p4_sdf', 'p4_conflict', 'p4_srf']:
       run_experiment(part, m, 1)
-#compare_heuristics_for_p4()
+compare_heuristics_for_p4()
 
 def compare_parts_small():
   for m in range(5, 10):
     for part in ['p1_sdf', 'p2_conflict', 'p4_sdf']:
       run_experiment(part, m, 25)
-#compare_parts_small()
+compare_parts_small()
 
 def find_all_min_solutions():
   number_of_queens = [0, 1, 1, 1, 3, 3, 4, 4, 5, 5, 5, 5, 7, 7, 7]
-  for m in range (2, 13):
+  for m in range (2, 10): # Too many files for larger m.
     for part in ['p1_all', 'p2_all', 'p4_all']:
       find_number_of_solutions(part, m, number_of_queens[m]) 
-find_all_min_solutions()
+#find_all_min_solutions()
 
 def compare_parts_large():
   for m in range(10, 15):
     for part in ['p1_sdf', 'p2_conflict', 'p4_sdf']:
       run_experiment(part, m, 5)
-#compare_parts_large()
+compare_parts_large()
--- graphs.r
+++ graphs.r
@@ -9,6 +9,47 @@ cilLow <- function(x) {
   return (result)
 }
 
+zero <-c(0)
+p <- 0
+plot_parts_time <- function(p_range, name, x_max, y_max) {
+  
+  c_range <-c(seq(1, length(p_range)))
+  plot(zero, zero, xlab="m", ylab="Time, s", log="y", main=name, pch=p, xlim=c(0, x_max), ylim=c(0.001, y_max), yaxt="n")
+  for (c in c_range) {
+    p_set <- subset(q, q$part == p_range[c])
+    lines(p_set$m, p_set$time, col=c)
+    points(p_set$m, p_set$time, col=c)
+  }
+  legend("topleft", title="parts", legend=p_range, col=c_range, cex=0.9, pch=1)
+  aty <- axTicks(2)
+  labels <- sapply(aty,function(i)
+    as.expression(bquote(.(i)))
+  )
+  axis(2,at=aty,labels=labels)
+}
+
+# Plot different p1 implementations.
+plot_parts_time(c('p1_atleast', 'p1_sum', 'p1_exists'), "Different p1 implementations", 13, 800)
+
+# Plot different p2 implementations.
+plot_parts_time(c('p2_atleast', 'p2_sum', 'p2_exists', 'p2_explicit'), "Different p2 implementations", 12, 300)
+
+# Plot different p1 heuristics.
+plot_parts_time(c('p1_atleast', 'p1_static', 'p1_sdf', 'p1_conflict', 'p1_srf'), "Different p1 heuristics", 13, 100)
+
+# Plot different p2 heuristics.
+plot_parts_time(c('p2_sum', 'p2_static', 'p2_sdf', 'p2_conflict', 'p2_srf'), "Different p2 heuristics", 13, 150)
+
+# Plot different p4 heuristics.
+plot_parts_time(c('p4_sum', 'p4_static', 'p4_sdf', 'p4_conflict', 'p4_srf'), "Different p4 heuristics", 13, 20)
+
+# Keep only best part for each.
+queries <- subset(queries, (queries$part=='p1_sdf' | queries$part=='p2_conflict' | queries$part=='p4_sdf'))
+
+# Collect results, calcute mean value for times and errors.
+q <- aggregate(queries[c("i", "time", "nodes", "queens")], by=queries[c("part", "m")], FUN=mean)
+q$time <- round(q$time, 3)
+
 q.cilLow <- aggregate(queries[c("time")], by=queries[c("part", "m")], FUN=cilLow)
 q$cilLow = round(q.cilLow$time, 4)
 
@@ -20,44 +61,15 @@ q$relError <- round(q$absError / q$time, 1) * 100
 
 q$cilUp <- q$time + q$absError
 
-plot_parts_time <- function(p_range, name, x_max, y_max) {
-  
-  p <- 0
-  zero <-c(0)
-  c_range <-c(seq(1, length(p_range)))
-  plot(zero, zero, xlab="m", ylab="Time, s", log="y", main=name, pch=p, xlim=c(0, x_max), ylim=c(0.001, y_max), yaxt="n")
-  for (c in c_range) {
-    p_set <- subset(q, q$part == p_range[c])
-    lines(p_set$m, p_set$time, col=c)
-    points(p_set$m, p_set$time, col=c)
-  }
-  legend("topleft", title="parts", legend=p_range, col=c_range, cex=0.5, pch=1)
-  aty <- axTicks(2)
-  labels <- sapply(aty,function(i)
-    as.expression(bquote(.(i)))
-  )
-  axis(2,at=aty,labels=labels)
-}
-
-# Plot different p1 implementations.
-plot_parts_time(c('p1_atleast', 'p1_sum', 'p1_exists'), "Different p1 implementations", 13, 1000)
-
-# Plot different p2 implementations.
-plot_parts_time(c('p2_atleast', 'p2_sum', 'p2_exists', 'p2_explicit'), "Different p2 implementations", 12, 1000)
-
-# Plot different p1 heuristics.
-plot_parts_time(c('p1_atleast', 'p1_static', 'p1_sdf', 'p1_conflict', 'p1_srf'), "Different p1 heuristics", 13, 1000)
-
-# Plot different p2 heuristics.
-plot_parts_time(c('p2_sum', 'p2_static', 'p2_sdf', 'p2_conflict', 'p2_srf'), "Different p2 heuristics", 13, 1000)
-
-# Plot different p4 heuristics.
-plot_parts_time(c('p4_sum', 'p4_static', 'p4_sdf', 'p4_conflict', 'p4_srf'), "Different p4 heuristics", 13, 1000)
+queries <- q
 
 # Plot different parts (best for each)
-plot_parts_time(c('p1_sdf', 'p2_conflict', 'p4_sdf'), "Best of each part", 13, 1000)
+plot_parts_time(c('p1_sdf', 'p2_conflict', 'p4_sdf'), "Different parts", 13, 70)
 
+# Plot number of nodes.
 plot(zero, zero, xlab="m", ylab="Nodes, no.", log="y", main="Nodes", pch=p, xlim=c(0, 13), ylim=c(1, 100000000), yaxt="n")
+p_range = c('p1_sdf', 'p2_conflict', 'p4_sdf')
+c_range <-c(seq(1, length(p_range)))
 for (c in c_range) {
   p_set <- subset(q, q$part == p_range[c])
   lines(p_set$m, p_set$nodes, col=c)
@@ -71,6 +83,22 @@ labels <- sapply(aty,function(i)
 axis(2,at=aty,labels=labels)
 
 plot(zero, zero, xlab="m", ylab="queens, no.", main="Number of queens", pch=p, xlim=c(0, 12), ylim=c(0, 7))
-p_set <- subset(q, q$part == "p1")
+p_set <- subset(q, q$part == "p1_sdf")
 lines(p_set$m, p_set$queens)
 points(p_set$m, p_set$queens)
+
+#write.table(q, file = "results.csv", sep = ",",
+            qmethod = "double")
+
+s <- read.table("number_of_solutions.txt", head = TRUE, sep=" ", fill=TRUE)
+
+# Plot number of solutions.
+plot(zero, zero, xlab="m", ylab="Nodes, no.", main="Number of solutions", pch=p, xlim=c(0, 10), ylim=c(1, 800))
+p_range = c('p1_all', 'p2_all', 'p4_all')
+c_range <-c(seq(1, length(p_range)))
+for (c in c_range) {
+  p_set <- subset(s, s$part == p_range[c])
+  lines(p_set$m, p_set$solutions_no, col=c)
+  points(p_set$m, p_set$solutions_no, col=c)
+}
+legend("topleft", title="parts", legend=p_range, col=c_range, cex=0.5, pch=1)
